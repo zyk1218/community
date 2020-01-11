83a6088a7679a34638c31495c37b4f6cfd8a31a6
remember <1977293486@qq.com>
2020-01-11 10:02:58 +0800

    init repo

---
+++ src/main/java/life/isip/community/controller/Hello.java
@@ -0,0 +1,19 @@
+package life.isip.community.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Hello {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
