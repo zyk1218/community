19ae5bba0a1961fade14b418e8b9ed2631af04a2
remember <1977293486@qq.com>
2020-01-25 21:47:21 +0800

    add publish page 1st version

---
+++ src/main/java/life/isip/community/controller/PublishController.java
@@ -0,0 +1,14 @@
+package life.isip.community.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PublishController {
+
+    @RequestMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+}
